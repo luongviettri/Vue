08dbda82519db967662ed6d446a80838eda2050b
luongviettri <96829347+luongviettri@users.noreply.github.com>
2023-12-21 16:24:41 +0700

    fixed bug jest dom related

--- tests/setup.js
+++ tests/setup.js
@@ -2,9 +2,9 @@ import "@testing-library/jest-dom/vitest";
 import { cleanup } from "@testing-library/vue";
 import { afterEach } from "vitest";
 
-import matchers from "@testing-library/jest-dom/types/matchers";
+// import matchers from "@testing-library/jest-dom/types/matchers";
 
-expect.extend(matchers);
+// expect.extend(matchers);
 
 afterEach(() => {
   cleanup();
--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -1,10 +1,12 @@
 import { render, screen } from "@testing-library/vue";
 import MainNav from "../../../src/components/MainNav.vue";
-import { describe } from "vitest";
+import { describe, expect } from "vitest";
 
 describe("MainNav", () => {
   it("displays company name", () => {
-    // render(MainNav).debug;
-    // screen.debug;
+    render(MainNav);
+    // screen.debug();
+    const companyName = screen.getByText("Fukuda AI");
+    expect(companyName).toBeInTheDocument();
   });
 });
