3f53be3e7bda1df8a9966dee51f84f0f7f29152c
luongviettri <96829347+luongviettri@users.noreply.github.com>
2023-12-21 15:39:31 +0700

    chuan bi sua jest dom

---
+++ tests/setup.js
@@ -0,0 +1,11 @@
+import "@testing-library/jest-dom/vitest";
+import { cleanup } from "@testing-library/vue";
+import { afterEach } from "vitest";
+
+import matchers from "@testing-library/jest-dom/types/matchers";
+
+expect.extend(matchers);
+
+afterEach(() => {
+  cleanup();
+});
---
+++ tests/unit/components/MainNav.test.js
@@ -0,0 +1,10 @@
+import { render, screen } from "@testing-library/vue";
+import MainNav from "../../../src/components/MainNav.vue";
+import { describe } from "vitest";
+
+describe("MainNav", () => {
+  it("displays company name", () => {
+    // render(MainNav).debug;
+    // screen.debug;
+  });
+});
